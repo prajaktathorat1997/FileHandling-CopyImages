d0e4aff85785b629726fadbc4935d924144e1108
prajaktathorat1997 <prajaktavthorat1997@gmail.com>
2024-04-12 09:33:42 +0530

    commited successfully

---
+++ ImageCopy.java
@@ -0,0 +1,37 @@
+package com.readbyte;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ImageCopy {
+
+	public ImageCopy() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		try {
+			FileInputStream fis=new FileInputStream("D:\\pratik.jpg");
+			File f=new File("D:\\MyImages");
+			f.mkdir();
+			File ff=new File(f,"prajakta.jpg");
+			
+				ff.createNewFile();
+				FileOutputStream p=new FileOutputStream(ff);
+			
+				byte[]a=fis.readAllBytes();
+				
+			p.write(a);
+			
+			
+		} catch (Exception e) {
+		
+			e.printStackTrace();
+		}
+
+	}
+
+}
